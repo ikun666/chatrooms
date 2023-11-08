18edf76f45d810a979de93c74fdfb596a00930c0
ikun666 <2922098031@qq.com>
2023-11-08 20:19:21 +0800

    add private msg

--- server/server.go
+++ server/server.go
@@ -15,6 +15,7 @@ type User struct {
 }
 
 var OnlineUserMap = make(map[string]User)
+var UserName2IdMap = make(map[string]string)
 var BroadcastChan = make(chan string)
 
 func main() {
@@ -40,6 +41,7 @@ func main() {
 		}
 		BroadcastChan <- fmt.Sprintf("user:%v go online", user.Name)
 		OnlineUserMap[user.ID] = user
+		UserName2IdMap[user.Name] = user.ID
 		go ClientRead(user)
 		go ClientWrite(user)
 	}
@@ -78,29 +80,45 @@ func Broadcast() {
 //		}
 //	}
 func ClientRead(user User) {
+	defer user.Conn.Close()
 	buf := make([]byte, 1024)
 	for {
 		n, err := user.Conn.Read(buf)
 		if err != nil {
 			fmt.Printf("user:%v read err:%v\n", user.Name, err)
 			delete(OnlineUserMap, user.ID)
+			delete(UserName2IdMap, user.Name)
 			BroadcastChan <- fmt.Sprintf("user:%v go offline", user.Name)
 			return
 		}
 		msg := string(buf[:n])
 
-		if len(msg) > 9 && msg[:8] == "--rename" { //--rename ikun
+		if len(msg) > 9 && msg[:9] == "--rename=" { //--rename=ikun
 			BroadcastChan <- fmt.Sprintf("%v rename: %v", user.Name, msg[9:])
 			user.Name = msg[9:]
 			OnlineUserMap[user.ID] = user
-		} else if msg == "--who" {
+			delete(UserName2IdMap, user.Name)
+			UserName2IdMap[user.Name] = user.ID
+		} else if msg == "--who" { //--who
 			users := make([]string, 0)
 			for k := range OnlineUserMap {
 				users = append(users, OnlineUserMap[k].Name)
 			}
 			msg = strings.Join(users, "\n")
 			user.Msg <- msg
-		} else {
+		} else if len(msg) > 7 && msg[:7] == "--name=" { //private --name=ikun hello world
+			msg1 := msg[7:]                  //ikun hello world
+			msg2 := strings.Split(msg1, " ") //ikun hello world
+
+			v, ok := UserName2IdMap[msg2[0]] //ikun
+			if ok && len(msg2) > 1 {
+				msg3 := strings.Join(msg2[1:], " ") //hello world
+				OnlineUserMap[v].Msg <- fmt.Sprintf("%v: %v", user.Name, msg3)
+			} else {
+				user.Msg <- fmt.Sprintf("name: %v not find or msg is null", msg2[0])
+			}
+
+		} else { //public
 			fmt.Printf("receive %v msg:%v\n", user.Name, msg)
 			BroadcastChan <- fmt.Sprintf("%v: %v", user.Name, msg)
 		}
@@ -109,6 +127,7 @@ func ClientRead(user User) {
 
 }
 func ClientWrite(user User) {
+	defer user.Conn.Close()
 	for {
 		msg := <-user.Msg
 		_, err := user.Conn.Write([]byte(msg))
